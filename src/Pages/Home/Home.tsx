0a4115fe538fb15b6884d184c903ae10a170678a
vanesora-serempre <83289071+vanesora-serempre@users.noreply.github.com>
2023-09-22 00:23:42 -0500

    add home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { HomeContainer } from './styles';
+import { WelcomeContainer } from './styles';
 
 
 export function HomePage() {
   return (
-    <HomeContainer>
-    </HomeContainer>
+    <WelcomeContainer>
+      <h1>Bienvenido</h1>
+      <img src={require('../../../../assets/img/logo.png')} alt="" />
+      <h2>El fracaso es la oportunidad de comenzar de nuevo, con más inteligencia</h2>
+    </WelcomeContainer>
   );
 }
--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -1,11 +1,38 @@
 import styled from 'styled-components';
 
-export const HomeContainer = styled.div`
-  height: 100%;
+export const WelcomeContainer = styled.div`
   width: 100%;
+  height: 100%;
+  background: linear-gradient(70deg, rgb(134, 132, 132) 5%, #0D0D0D);
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
-  overflow: hidden;
+
+  img {
+    width: 350px;
+    max-width: 90%;
+  }
+
+  h1 {
+    margin-top: 20px;
+    color: white;
+    font-size: 60px;
+
+    @media (max-width: 460px) {
+      font-size: 40px;
+    }
+  }
+
+  h2 {
+    margin-top: 10px;
+    color: white;
+    font-size: 25px;
+    text-align: center;
+    line-height: normal;
+
+    @media (max-width: 460px) {
+      font-size: 18px;
+    }
+  }
 `;
