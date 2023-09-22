04c4bb33ebf9a8b6a4d87cbcd27ed87ae4295030
vanesora-serempre <83289071+vanesora-serempre@users.noreply.github.com>
2023-09-22 00:17:41 -0500

    add table

--- src/Pages/UserList/UserList.tsx
+++ src/Pages/UserList/UserList.tsx
@@ -1,11 +1,235 @@
 import React, { FunctionComponent, useEffect, useState } from "react";
+import {
+  Table,
+  TableBox,
+  TableContainer,
+  TableRow,
+  TableHeader,
+  TableCell,
+  ContainerActions,
+  NoDataMessage,
+  Paginator,
+} from "./styles";
 
 export function UserListPage() {
+  const [actualPage, setActualPage] = useState(1);
+  const headers = [
+    "PrimerNombre",
+    "PrimerApellido",
+    "SegundoApellido",
+    "Celular",
+    "Perfil",
+  ];
 
+  const data: { [key: string]: string | number | boolean }[] = [
+    {
+      id: 1,
+      PrimerNombre: "Fabian",
+      PrimerApellido: "Cardenas",
+      SegundoApellido: "Colorado",
+      Celular: "3154879658",
+      Perfil: "Cleaner",
+      status: true,
+    },
+    {
+      id: 2,
+      PrimerNombre: "Camilo",
+      PrimerApellido: "Quijano",
+      SegundoApellido: "Contreras",
+      Celular: "3154789652",
+      Perfil: "Cleaner",
+      status: true,
+    },
+    {
+      id: 3,
+      PrimerNombre: "Freddy",
+      PrimerApellido: "Arcila",
+      SegundoApellido: "Sierra",
+      Celular: "3598741458",
+      Perfil: "Cleaner",
+      status: true,
+    },
+    {
+      id: 4,
+      PrimerNombre: "Felipe",
+      PrimerApellido: "Torres",
+      SegundoApellido: "Muñoz",
+      Celular: "3124516895",
+      Perfil: "Cleaner",
+      status: true,
+    },
+  ];
+
+  const actions = {
+    delete: false,
+    edit: true,
+    view: true,
+    status: false,
+    add: false,
+    substract: false,
+  };
+
+  const totalPages = 3;
+
+  const handleBack = () => {
+    setActualPage(actualPage - 1);
+  };
+
+  const handleNext = () => {
+    setActualPage(actualPage + 1);
+  };
+
+  const handleAction = (action: string, item: any) => {
+    console.log("click action");
+  };
+  const viewId = false;
+  const viewStatus = false;
+
+  let keys = Object.keys(data[0] || {}).filter(
+    (item) => item !== "select" && item !== "notSelect" && item !== "alarm"
+  );
+  if (!viewId) keys = keys.filter((item) => item !== "id");
+  if (!viewStatus) keys = keys.filter((item) => item !== "status");
 
   return (
-    <div style={{ backgroundColor: "white", height:'100%', padding: '100px 20px' }}>
+    <TableBox>
+      <TableContainer>
+        <div style={{ overflowX: "auto", paddingBottom: "10px" }}>
+          {data && data.length > 0 ? (
+            <Table>
+              <TableRow height={"40px"}>
+                {headers.map((header: any, i: number) => (
+                  <TableHeader
+                    key={i}
+                    headersLength={headers.length}
+                    height={"40px"}
+                    color={""}
+                    colorText={""}
+                    positionHeader={""}
+                    border={""}
+                  >
+                    <div className="div-th">{header}</div>
+                  </TableHeader>
+                ))}
+              </TableRow>
 
-    </div>
+              {data.map((item, i) => (
+                <TableRow key={i} height={"40px"}>
+                  {keys.map((key, y) => (
+                    <TableCell
+                      key={y}
+                      headersLength={headers.length}
+                      height={"40px"}
+                      border={""}
+                      positionCells={""}
+                    >
+                      <div className="div-td">{item[key]}</div>
+                    </TableCell>
+                  ))}
+                  <ContainerActions>
+                    {actions.add && (
+                      <div className="container-icon">
+                        <button
+                          className="icon-add"
+                          style={{ transform: "scale(0.8)" }}
+                          onClick={() => handleAction("add", item)}
+                          disabled={!item.status}
+                        >
+                          <span className="material-icons-outlined">add</span>
+                        </button>
+                      </div>
+                    )}
+                    {actions.substract && (
+                      <div className="container-icon">
+                        <button
+                          className="icon-subs"
+                          style={{ transform: "scale(0.8)" }}
+                          onClick={() => handleAction("substrac", item)}
+                          disabled={!item.status}
+                        >
+                          <span className="material-icons-outlined">
+                            remove
+                          </span>
+                        </button>
+                      </div>
+                    )}
+                    {actions.view && (
+                      <div className="container-icon">
+                        <button
+                          className="icon"
+                          style={{ transform: "scale(0.8)" }}
+                          onClick={() => handleAction("view", item)}
+                        >
+                          <span>visibility</span>
+                        </button>
+                      </div>
+                    )}
+                    {actions.edit && (
+                      <div className="container-icon">
+                        <button
+                          className="icon"
+                          style={{ transform: "scale(0.8)" }}
+                          onClick={() => handleAction("edit", item)}
+                          disabled={!item.status}
+                        >
+                          <span className="material-icons-outlined">edit</span>
+                        </button>
+                      </div>
+                    )}
+                    {actions.status && (
+                      <div className="container-icon">
+                        <label className="container-switch">
+                          <input
+                            type="checkbox"
+                            checked={Boolean(item.status)}
+                            onChange={() => handleAction("status", item)}
+                          />
+                          <span className="slider round"></span>
+                        </label>
+                      </div>
+                    )}
+                    {actions.delete && (
+                      <div className="container-icon">
+                        <button
+                          className="icon"
+                          style={{ transform: "scale(0.8)" }}
+                          onClick={() => handleAction("delete", item)}
+                          disabled={!item.status}
+                        >
+                          <span className="material-icons-outlined">
+                            delete
+                          </span>
+                        </button>
+                      </div>
+                    )}
+                  </ContainerActions>
+                </TableRow>
+              ))}
+            </Table>
+          ) : (
+            <NoDataMessage>No se encontraron datos relacionados</NoDataMessage>
+          )}
+          (
+          <Paginator>
+            <span
+              className={`material-icons ${actualPage === 1 ? "disabled" : ""}`}
+              onClick={handleBack}
+            >
+              arrow_circle_left
+            </span>
+            <h3>{`Página ${actualPage} de ${totalPages}`}</h3>
+            <span
+              className={`material-icons ${
+                actualPage >= totalPages ? "disabled" : ""
+              }`}
+              onClick={handleNext}
+            >
+              arrow_circle_right
+            </span>
+          </Paginator>
+          )
+        </div>
+      </TableContainer>
+    </TableBox>
   );
 }
--- src/Pages/UserList/styles.ts
+++ src/Pages/UserList/styles.ts
@@ -1,5 +1,125 @@
 import styled from 'styled-components';
 
-export const AgeGateContainer = styled.div`
-  height: 100vh;
+export const TableBox = styled.div`
+  overflow-x: hidden;
+  overflow-y: hidden;
+`;
+
+export const TableContainer = styled.div`
+  margin: 5px;
+`;
+
+export const Table = styled.table`
+  border-collapse: collapse;
+  border-radius: 10px;
+  border-style: hidden;
+  width: 100%;
+`;
+
+export const TableHeader = styled.th<{headersLength: any, height: any, colorText: string, positionHeader: string, border: string}>`
+  border-style: none;
+  margin: 0;
+  padding: 0;
+  width: calc(100% / ${(props) => props.headersLength});
+  height: ${(props) => (props.height ? props.height : '40px')};
+
+  .div-th {
+    padding: 8px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    background-color: ${(props) => (props.color ? props.color : '#FF7300')};
+    color: ${(props) => (props.colorText ? props.colorText : 'white')};
+    text-align: ${(props) => props.positionHeader};
+    border: ${(props) => (props.border ? props.border : '1px solid #FF7300')};
+    border-color: ${(props) => (props.color ? props.color : '#FF7300')} !important;
+
+    &.border-start-start {
+      border-top-left-radius: 10px;
+    }
+
+    &.border-start-end {
+      border-top-right-radius: 10px;
+    }
+  }
+`;
+
+export const TableRow = styled.tr<{height: any}>`
+  width: 100%;
+  height: ${(props) => (props.height ? props.height : '40px')};
+`;
+
+export const TableCell = styled.td<{border: string, headersLength: any, positionCells:string}>`
+  border-style: none;
+  margin: 0;
+  padding: 0;
+  width: calc(100% / ${(props) => props.headersLength});
+  height: ${(props) => (props.height ? props.height : '40px')};
+
+  .div-td {
+    padding: 8px;
+    width: 100%;
+    height: 100%;
+    border: ${(props) => (props.border ? props.border : '1px solid #FF7300')};
+    text-align: ${(props) => props.positionCells};
+    background-color: ${(props) => ('transparent')};
+    color: ${(props) => ('inherit')};
+
+    &.border-end-start {
+      border-bottom-left-radius: 10px;
+    }
+
+    &.border-end-end {
+      border-bottom-right-radius: 10px;
+    }
+  }
+`;
+
+export const ContainerActions = styled.td`
+  display: flex;
+  align-items: center;
+  height: 100%;
+
+  .container-icon {
+    height: 100%;
+    margin: 5px;
+    display: flex;
+    align-items: center;
+
+    .icon-add {
+      background-color: ${(props) => '#33691E'};
+    }
+
+    .icon-subs {
+      background-color: ${(props) => 'rgb(194, 8, 8)'};
+    }
+  }
+`;
+
+export const NoDataMessage = styled.div`
+  font-size: 20px;
+  color: ${(props) => '#090088'};
+  text-align: center;
+  border: 2px solid ${(props) => '#FF7300'};
+  padding: 20px;
+  border-radius: 10px;
+`;
+
+export const Paginator = styled.div`
+  display: flex;
+  margin: 10px 0;
+  align-items: center;
+
+  h3 {
+    margin: 0 5px;
+  }
+
+  .mat-icon {
+    cursor: pointer;
+
+    &.disabled {
+      color: gray !important;
+    }
+  }
 `;
